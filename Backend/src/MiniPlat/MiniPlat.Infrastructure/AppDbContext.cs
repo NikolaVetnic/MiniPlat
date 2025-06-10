cc1f22d47a0eb25cbe0a9820f6b5b9ee9c7d5839
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2025-06-10 09:42:58 +0200

    ✨ add infrastructure services (and openiddict)

---
+++ Backend/src/MiniPlat/MiniPlat.Infrastructure/AppDbContext.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MiniPlat.Domain.Models;
+
+namespace MiniPlat.Infrastructure;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<ApplicationUser, IdentityRole, string>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        // Apply all configurations taken from classes that implement IEntityTypeConfiguration<>
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        builder.UseOpenIddict();
+    }
+}
---
+++ Backend/src/MiniPlat/MiniPlat.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,87 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MiniPlat.Domain.Models;
+using OpenIddict.Abstractions;
+
+namespace MiniPlat.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services
+            .AddAppDbContext(configuration)
+            .AddOpenIddictServer()
+            .AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+        return services;
+    }
+
+    private static IServiceCollection AddAppDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+            options.UseOpenIddict();
+        });
+        
+        return services;
+    }
+    
+    private static IServiceCollection AddOpenIddictServer(this IServiceCollection services)
+    {
+        services.AddOpenIddict()
+            .AddCore(options =>
+            {
+                options.UseEntityFrameworkCore()
+                    .UseDbContext<AppDbContext>();
+            })
+            .AddServer(options =>
+            {
+                options.RegisterScopes(
+                    OpenIddictConstants.Scopes.OpenId,
+                    OpenIddictConstants.Scopes.Email,
+                    OpenIddictConstants.Scopes.Profile,
+                    OpenIddictConstants.Scopes.OfflineAccess,
+                    "api");
+
+                options
+                    .SetAccessTokenLifetime(TimeSpan.FromMinutes(60))
+                    .SetRefreshTokenLifetime(TimeSpan.FromDays(14))
+                    .UseReferenceRefreshTokens();
+
+                options.RegisterClaims(
+                    OpenIddictConstants.Claims.Email,
+                    OpenIddictConstants.Claims.Name,
+                    "fullName");
+
+                options.SetTokenEndpointUris("/api/Auth/Token");
+                options.SetUserInfoEndpointUris("/api/Auth/UserInfo");
+
+                options.AllowPasswordFlow();
+                options.AllowRefreshTokenFlow();
+                options.AcceptAnonymousClients();
+
+                options.AddEphemeralEncryptionKey()
+                    .AddEphemeralSigningKey();
+
+                options.UseAspNetCore()
+                    .EnableTokenEndpointPassthrough()
+                    .EnableUserInfoEndpointPassthrough();
+            })
+            .AddValidation(options =>
+            {
+                options.UseLocalServer();
+                options.UseAspNetCore();
+            });
+
+        return services;
+    }
+}
