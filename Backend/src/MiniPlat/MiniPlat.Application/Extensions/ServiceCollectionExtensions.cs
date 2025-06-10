01c6ec0a48c6a2d5db862d3e4181c5e81845fe0f
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2025-06-10 09:40:59 +0200

    ✨ add application services

---
+++ Backend/src/MiniPlat/MiniPlat.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using MiniPlat.Application.Behaviors;
+
+namespace MiniPlat.Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+        
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddHttpContextAccessor();  
+        
+        // services.AddScoped<ICurrentUser, CurrentUser>();
+        // services.AddScoped<ITokenService, TokenService>();
+        
+        return services;
+    }
+}
