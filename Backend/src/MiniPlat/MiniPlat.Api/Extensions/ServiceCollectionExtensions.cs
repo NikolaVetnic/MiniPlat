6e38328115c67d2632dcc27a59760b6dcf427c6c
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2025-06-10 09:42:29 +0200

    ✨ add api services

---
+++ Backend/src/MiniPlat/MiniPlat.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,9 @@
+namespace MiniPlat.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services;
+    }
+}
