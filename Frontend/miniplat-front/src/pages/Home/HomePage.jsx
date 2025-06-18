d97aa43af42c7ded0f9466e0ac9eb04dc624ab7b
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2025-06-19 00:16:02 +0200

    ✨ updated admin subject card display

--- Frontend/miniplat-front/src/pages/Home/HomePage.jsx
+++ Frontend/miniplat-front/src/pages/Home/HomePage.jsx
@@ -86,6 +86,7 @@ const HomePage = ({ onLogout }) => {
                         semester={subject.semester}
                         lecturerUsername={subject.lecturer}
                         assistantUsername={subject.assistant}
+                        isActive={subject.isActive}
                       />
                     ))}
                   </div>
